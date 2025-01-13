d897306a976ca84f09e8554b3abf9bd78bbe00cf
margusveeber <margus.veeber@gmail.com>
2025-01-13 20:41:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ function updateClock() {
     const now = new Date();
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
+    const day = now.getDate();
+    const month = now.toLocaleString('en-US', { month: 'short' });
+    
+    document.getElementById('date').innerHTML = `<span class="day">${day}.</span> <span class="month">${month}</span>`;
     document.getElementById('clock').innerHTML = `${hours}<span class="colon">:</span>${minutes}`;
 }
 
